745344ecdb7aa2258e19a34c613fdfcc2951195d
Rominapol <143038838+Rominapol@users.noreply.github.com>
2023-08-27 21:53:28 -0300

    Add files via upload

---
+++ libros.py
@@ -0,0 +1,17 @@
+class Libro
+	def __init__(self, titulo, autor, genero,puntuacion)
+		self.titulo=titulo
+		self.autor=autor
+		self.genero=genero
+		self.puntuacion=puntuacion
+	
+lista_libros=[libro1 = Libro("Cien años de soledad", "Gabriel García Márquez", "Ficción", 4.5)
+libro2 = Libro("1984", "George Orwell", "Ciencia Ficción", 4.3)
+libro3 = Libro("El Hobbit", "J.R.R. Tolkien", "Fantasía", 4.7)
+libro4 = Libro("Orgullo y Prejuicio", "Jane Austen", "Romance", 4.2)
+libro5 = Libro("Crimen y Castigo", "Fiódor Dostoyevski", "Clásico", 4.4)
+libro6 = Libro("Los Juegos del Hambre", "Suzanne Collins", "Juvenil", 4.1)
+libro7 = Libro("Don Quijote de la Mancha", "Miguel de Cervantes", "Clásico", 4.6)
+libro8 = Libro("Harry Potter y la Piedra Filosofal", "J.K. Rowling", "Fantasía", 4.8)
+libro9 = Libro("Los Pilares de la Tierra", "Ken Follett", "Histórica", 4.4)
+libro10 = Libro("Cazadores de Sombras: Ciudad de Hueso", "Cassandra Clare", "Fantasía", 4.0)]
